a87be58dc77a725a93f1697bb1baeb5e9e6f31f9
Grant Menke <grantmenke@google.com>
2025-04-22 15:18:46 +0000

    Resolve cross account user ringtone validation.
    Resolves a vulnerability found with the lack of cross account user ringtone
    validation in RingtoneFactory. The reporter found that a ringtone file owned by a different user can be accessed and played by the user who does not own that file. This fix also prevents a regression that was caused by the original fix for this issue.
    
    Bug: 356604577
    Flag: EXEMPT Critical CVE bugfix
    Test: RingtoneFactoryTest
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:294327c63301a330c87efb79e741e4f505f78312
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:81f00f98757e0de71d686e3651c6fa8540b254fd
    Merged-In: I8687470f3be2f68f10cc20f684f401ef4bd9357b
    Change-Id: I8687470f3be2f68f10cc20f684f401ef4bd9357b

--- src/com/android/server/telecom/Call.java
+++ src/com/android/server/telecom/Call.java
@@ -3532,7 +3532,8 @@ public class Call implements CreateConnectionResponse, EventManager.Loggable,
         return Contacts.getLookupUri(mCallerInfo.getContactId(), mCallerInfo.lookupKey);
     }
 
-    Uri getRingtone() {
+    @VisibleForTesting
+    public Uri getRingtone() {
         return mCallerInfo == null ? null : mCallerInfo.contactRingtoneUri;
     }
 
